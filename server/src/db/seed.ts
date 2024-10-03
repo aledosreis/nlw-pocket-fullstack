64478364313ebe5270e1a6fd3ee263e1fbeaf1a8
Alessandro Reis <alessandrodreis23@gmail.com>
2024-10-03 16:59:35 -0300

    auth: change seed file to create test user

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,25 +1,33 @@
 import { client, db } from '.'
-import { goalCompletions, goals } from './schema'
+import { goalCompletions, goals, users } from './schema'
 import dayjs from 'dayjs'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
+  await db.delete(users)
 
-  const result = await db
+  const insertedUser = await db
+    .insert(users)
+    .values(
+      { username: 'teste', email: 'teste@teste.com', password: 'teste' }
+    )
+    .returning()
+
+  const insertedGoals = await db
     .insert(goals)
     .values([
-      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-      { title: 'Me exercitar', desiredWeeklyFrequency: 3 },
-      { title: 'Meditar', desiredWeeklyFrequency: 1 },
+      { title: 'Acordar cedo', desiredWeeklyFrequency: 5, userId: insertedUser[0].id },
+      { title: 'Me exercitar', desiredWeeklyFrequency: 3, userId: insertedUser[0].id },
+      { title: 'Meditar', desiredWeeklyFrequency: 1, userId: insertedUser[0].id },
     ])
     .returning()
 
   const startOfWeek = dayjs().startOf('week')
 
   await db.insert(goalCompletions).values([
-    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
-    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+    { goalId: insertedGoals[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: insertedGoals[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
   ])
 }
 
