a658cb2da962200f66877e8e852510d67088e65b
Alessandro Reis <alessandrodreis23@gmail.com>
2024-10-04 10:00:34 -0300

    auth: change create-user function to check if email or username is already registered

--- server/src/functions/create-user.ts
+++ server/src/functions/create-user.ts
@@ -1,5 +1,6 @@
-import { db } from "../db"
-import { users } from "../db/schema"
+import { eq } from 'drizzle-orm'
+import { db } from '../db'
+import { users } from '../db/schema'
 
 interface CreateUserRequest {
   username: string
@@ -10,24 +11,56 @@ interface CreateUserRequest {
 export async function createUser({
   username,
   email,
-  password
+  password,
 }: CreateUserRequest) {
+  const usernameAlreadyRegistered = await db
+    .select({
+      id: users.id,
+      username: users.username,
+      email: users.email,
+      createdAt: users.createdAt,
+    })
+    .from(users)
+    .where(eq(users.username, username))
+
+  if (usernameAlreadyRegistered.length > 0) {
+    return {
+      error: 'Username already registered.',
+    }
+  }
+
+  const emailAlreadyRegistered = await db
+    .select({
+      id: users.id,
+      username: users.username,
+      email: users.email,
+      createdAt: users.createdAt,
+    })
+    .from(users)
+    .where(eq(users.email, email))
+
+  if (emailAlreadyRegistered.length > 0) {
+    return {
+      error: 'Email already registered.',
+    }
+  }
+
   const result = await db
     .insert(users)
     .values({
       username,
       email,
-      password
+      password,
     })
     .returning()
 
-    const createdUser = result[0]
+  const createdUser = result[0]
 
-    return {
-      createdUser: {
-        username: createdUser.email,
-        email: createdUser.email,
-        createdAt: createdUser.createdAt,
-      }
-    }
+  return {
+    createdUser: {
+      username: createdUser.email,
+      email: createdUser.email,
+      createdAt: createdUser.createdAt,
+    },
+  }
 }
