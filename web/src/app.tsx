3411f556e839cf9bd85daf75b3e0a47cc1f54987
Alessandro Reis <alessandrodreis23@gmail.com>
2024-10-04 17:35:30 -0300

    move page content from app to goals file

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,22 +1,5 @@
-import { useQuery } from '@tanstack/react-query'
-import { Dialog } from '@/components/ui/dialog'
-import { CreateGoal } from '@/components/create-goal'
-import { Summary } from '@/components/summary'
-import { EmptyGoals } from '@/components/empty-goals'
-import { getSummary } from '@/http/get-summary'
+import { Goals } from '@/pages/goals'
 
 export function App() {
-  const { data } = useQuery({
-    queryKey: ['summary'],
-    queryFn: getSummary,
-    staleTime: 1000 * 60, // 60 sec
-  })
-
-  return (
-    <Dialog>
-      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
-
-      <CreateGoal />
-    </Dialog>
-  )
+  return <Goals />
 }
