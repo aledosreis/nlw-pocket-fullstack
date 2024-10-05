cdf9eb17a49cdd661c34271f1460f298c418982d
Alessandro Reis <alessandrodreis23@gmail.com>
2024-10-05 14:48:27 -0300

    auth: add logout function

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle2, Plus } from 'lucide-react'
+import { CheckCircle2, LogOut, Plus } from 'lucide-react'
 import { useQuery } from '@tanstack/react-query'
 import { Progress, ProgressIndicator } from './ui/progress-bar'
 import { DialogTrigger } from './ui/dialog'
@@ -9,13 +9,19 @@ import { getSummary } from '@/http/get-summary'
 import dayjs from 'dayjs'
 import ptBR from 'dayjs/locale/pt-br'
 import { PendingGoals } from './pending-goals'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { getUserData } from '@/http/get-user-data'
 
 dayjs.locale(ptBR)
 
 export function Summary() {
   const { username } = useParams()
+  const navigate = useNavigate()
+
+  function handleLogout() {
+    localStorage.removeItem('@in.orbit/token')
+    navigate('/')
+  }
 
   const { data: userData } = useQuery({
     queryKey: ['userData'],
@@ -39,90 +45,102 @@ export function Summary() {
   const completedPercentage = Math.round((data.completed * 100) / data.total)
 
   return (
-    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <InOrbitIcon />
-          <span className="text-lg font-semibold capitalize">
-            {firstDayOfWeek} - {lastDayOfWeek}
-          </span>
+    <div className='w-screen'>
+      <div className='px-10 pt-4 flex justify-between items-center absolute w-full'>
+        <span className='text-lg font-semibold'>
+          {userData?.username}{' '}
+          <span className='text-base font-normal text-zinc-400'>({userData?.email})</span>
+        </span>
+        <Button size='sm' onClick={handleLogout}>
+          <LogOut className='size-4' />
+          Sair
+        </Button>
+      </div>
+      <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <InOrbitIcon />
+            <span className="text-lg font-semibold capitalize">
+              {firstDayOfWeek} - {lastDayOfWeek}
+            </span>
+          </div>
+
+          <DialogTrigger asChild>
+            <Button size="sm">
+              <Plus className="size-4" />
+              Cadastrar meta
+            </Button>
+          </DialogTrigger>
         </div>
 
-        <DialogTrigger asChild>
-          <Button size="sm">
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
+        <div className="flex flex-col gap-3">
+          <Progress value={8} max={15}>
+            <ProgressIndicator style={{ width: `${completedPercentage}%` }} />
+          </Progress>
 
-      <div className="flex flex-col gap-3">
-        <Progress value={8} max={15}>
-          <ProgressIndicator style={{ width: `${completedPercentage}%` }} />
-        </Progress>
-
-        <div className="flex items-center justify-between text-xs text-zinc-400">
-          <span>
-            Você completou{' '}
-            <span className="text-zinc-100">{data?.completed}</span> de{' '}
-            <span className="text-zinc-100">{data?.total}</span> metas nessa
-            semana.
-          </span>
-          <span>{completedPercentage}%</span>
+          <div className="flex items-center justify-between text-xs text-zinc-400">
+            <span>
+              Você completou{' '}
+              <span className="text-zinc-100">{data?.completed}</span> de{' '}
+              <span className="text-zinc-100">{data?.total}</span> metas nessa
+              semana.
+            </span>
+            <span>{completedPercentage}%</span>
+          </div>
         </div>
-      </div>
 
-      <Separator />
+        <Separator />
 
-      <PendingGoals />
+        <PendingGoals />
 
-      <div className="flex flex-col gap-6">
-        <h2 className="text-xl font-medium">Sua semana</h2>
+        <div className="flex flex-col gap-6">
+          <h2 className="text-xl font-medium">Sua semana</h2>
 
-        {data.completed === 0 ? (
-          <span className="text-sm text-zinc-400">
-            Você ainda não completou nenhuma meta essa semana.
-          </span>
-        ) : null}
+          {data.completed === 0 ? (
+            <span className="text-sm text-zinc-400">
+              Você ainda não completou nenhuma meta essa semana.
+            </span>
+          ) : null}
 
-        {data.goalsPerDay &&
-          Object.entries(data.goalsPerDay).map(([date, goals]) => {
-            const weekDay = dayjs(date).format('dddd')
-            const formattedDate = dayjs(date).format('D [de] MMMM')
+          {data.goalsPerDay &&
+            Object.entries(data.goalsPerDay).map(([date, goals]) => {
+              const weekDay = dayjs(date).format('dddd')
+              const formattedDate = dayjs(date).format('D [de] MMMM')
 
-            return (
-              <div key={date} className="flex flex-col gap-4">
-                <h3 className="font-medium">
-                  <span className="capitalize">{weekDay}</span>{' '}
-                  <span className="text-zinc-400 text-xs">
-                    ({formattedDate})
-                  </span>
-                </h3>
+              return (
+                <div key={date} className="flex flex-col gap-4">
+                  <h3 className="font-medium">
+                    <span className="capitalize">{weekDay}</span>{' '}
+                    <span className="text-zinc-400 text-xs">
+                      ({formattedDate})
+                    </span>
+                  </h3>
 
-                <ul className="flex flex-col gap-3">
-                  {goals.map(goal => {
-                    const time = dayjs(goal.completedAt).format('HH:mm')
+                  <ul className="flex flex-col gap-3">
+                    {goals.map(goal => {
+                      const time = dayjs(goal.completedAt).format('HH:mm')
 
-                    return (
-                      <li key={goal.id} className="flex items-center gap-2">
-                        <CheckCircle2 className="size-4 text-pink-500" />
-                        <span className="text-sm text-zinc-400">
-                          Você completou "
-                          <span className="text-zinc-100 font-medium">
-                            {goal.title}
+                      return (
+                        <li key={goal.id} className="flex items-center gap-2">
+                          <CheckCircle2 className="size-4 text-pink-500" />
+                          <span className="text-sm text-zinc-400">
+                            Você completou "
+                            <span className="text-zinc-100 font-medium">
+                              {goal.title}
+                            </span>
+                            " às{' '}
+                            <span className="text-zinc-100 font-medium">
+                              {time}h
+                            </span>
                           </span>
-                          " às{' '}
-                          <span className="text-zinc-100 font-medium">
-                            {time}h
-                          </span>
-                        </span>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )
-          })}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              )
+            })}
+        </div>
       </div>
     </div>
   )
