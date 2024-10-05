c1237ef2e20abf5e45fd1ba8fef096f6ec753a82
Alessandro Reis <alessandrodreis23@gmail.com>
2024-10-05 15:40:39 -0300

    change home layout

--- web/src/pages/landing.tsx
+++ web/src/pages/landing.tsx
@@ -19,16 +19,18 @@ export function Landing() {
   }, [])
   
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <div className="flex flex-col gap-6 max-w-[480px]">
-        <div className="flex gap-3 items-center">
-          <InOrbitIcon />
-          <span className="text-2xl font-semibold">in.Orbit</span>
+    <div className="h-screen w-screen flex items-center">
+      <div className="flex justify-between w-full h-full p-10">
+        <div className="flex flex-1 justify-center items-center flex-col gap-6">
+          <div className="flex gap-3 items-center">
+            <InOrbitIcon />
+            <span className="text-2xl font-semibold">in.Orbit</span>
+          </div>
+
+          <span className="text-lg font-medium">Registre e controle suas metas semanais</span>
         </div>
 
-        <span className="text-lg font-medium">Registre e controle suas metas semanais</span>
-
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col gap-4 flex-1 items-center justify-center">
           <Link to='/login' className="flex items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 bg-violet-500 text-violet-50 hover:bg-violet-600 ring-violet-500 px-4 py-2.5">
             <LogIn className="size-4" />
             Faça seu login
