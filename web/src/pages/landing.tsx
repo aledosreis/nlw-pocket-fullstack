a4f6089fc5803ed68818338109d5b4b0379ded5c
Alessandro Reis <alessandrodreis23@gmail.com>
2024-10-05 09:49:00 -0300

    add home page

---
+++ web/src/pages/landing.tsx
@@ -0,0 +1,23 @@
+import { InOrbitIcon } from "@/components/in-orbit-icon";
+import { Link } from "react-router-dom";
+
+export function Landing() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="flex flex-col gap-6 max-w-[480px]">
+        <div className="flex gap-3 items-center">
+          <InOrbitIcon />
+          <span className="text-2xl font-semibold">in.Orbit</span>
+        </div>
+
+        <span className="text-lg font-medium">Registre e controle suas metas semanais</span>
+
+        <div className="flex justify-between items-center">
+          <Link to='/login' className="flex items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 bg-violet-500 text-violet-50 hover:bg-violet-600 ring-violet-500 px-4 py-2.5">Faça seu login</Link>
+          <span className="text-zinc-400 text-sm">-- OU --</span>
+          <Link to='/register' className="flex items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 bg-violet-500 text-violet-50 hover:bg-violet-600 ring-violet-500 px-4 py-2.5">Cadastre-se</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "@/pages/landing";
 import { Login } from "@/pages/login";
 import { Register } from "@/pages/register";
 import { Goals } from "@/pages/goals";
@@ -7,6 +8,7 @@ export function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/:username" element={<Goals />} />
