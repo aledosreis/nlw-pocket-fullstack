9606b6f852fe283e9bfc9c17a8e0b6c7944caae5
Alessandro Reis <alessandrodreis23@gmail.com>
2024-10-05 10:58:39 -0300

    auth: add user login and redirect to goals page

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,9 +1,48 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from '@/components/ui/button'
 import { InOrbitIcon } from '@/components/in-orbit-icon'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import { authenticateUser } from '@/http/authenticate-user'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const loginForm = z.object({
+  username: z.string(),
+  password: z.string(),
+})
+
+type LoginForm = z.infer<typeof loginForm>
 
 export function Login() {
+  const navigate = useNavigate()
+  const {register, handleSubmit} = useForm<LoginForm>({
+    resolver: zodResolver(loginForm),
+  })
+
+  useEffect(() => {
+    const token = localStorage.getItem('@in.orbit/token')
+
+    if (token) {
+      // Buscar informação do usuario logado
+      // navigate('')
+      console.log('Autenticado')
+    }
+  }, [])
+
+  async function handleLogin(data: LoginForm) {
+    const {authenticatedUser: { username }, token} = await authenticateUser({
+      username: data.username,
+      password: data.password,
+    })
+
+    localStorage.setItem('@in.orbit/token', token)
+
+    navigate(`/${username}`)
+  }
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="py-10 max-w-[480px] w-full flex flex-col gap-6">
@@ -13,7 +52,7 @@ export function Login() {
         </div>
 
         <form
-          // onSubmit={handleSubmit(handleCreateGoal)}
+          onSubmit={handleSubmit(handleLogin)}
           className="flex-1 flex flex-col gap-6 justify-between"
         >
           <div className="flex flex-col gap-6">
@@ -23,7 +62,7 @@ export function Login() {
                 id="username"
                 autoFocus
                 placeholder="Username"
-                // {...register('title')}
+                {...register('username')}
               />
             </div>
             <div className="flex flex-col gap-2">
@@ -32,7 +71,7 @@ export function Login() {
                 id="password"
                 placeholder="Password"
                 type="password"
-                // {...register('title')}
+                {...register('password')}
               />
             </div>
           </div>
